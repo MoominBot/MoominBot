9b696266c41d6b7bccb38977a517312b9a0b2b89
DevAndromeda <46562212+DevAndromeda@users.noreply.github.com>
2021-11-24 19:05:52 +0545

    feat: add rashifal map

--- src/interactions/slash/General/RashifalCommand.ts
+++ src/interactions/slash/General/RashifalCommand.ts
@@ -1,5 +1,20 @@
 import { ApplicationCommandData } from "discord.js";
 
+export enum RashifalMap {
+    "मेष" = "ARIES",
+    "वृष" = "TAURUS",
+    "मिथुन" = "GEMINI",
+    "कर्कट" = "CANCER",
+    "सिंह" = "LEO",
+    "कन्या" = "VIRGO",
+    "तुला" = "LIBRA",
+    "वृश्चिक" = "SCORPIO",
+    "धनु" = "SAGITTARIUS",
+    "मकर" = "CAPRICORN",
+    "कुम्भ" = "AQUARIUS",
+    "मीन" = "PISCES"
+}
+
 const commandConfig = {
     name: "rashifal",
     description: "Shows Rashifal",

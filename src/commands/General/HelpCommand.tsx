64e239a8e1025e0c54d6acdf2c2ce8232f2db6a7
DevAndromeda <46562212+DevAndromeda@users.noreply.github.com>
2021-12-04 13:03:41 +0545

    refactor(HelpCommand): add category filter

--- src/commands/General/HelpCommand.tsx
+++ src/commands/General/HelpCommand.tsx
@@ -5,10 +5,14 @@ import {
     Client,
     Collection,
     CommandInteraction,
+    Guild,
     InteractionCollector,
     Message,
     MessageEmbed,
-    Snowflake
+    Snowflake,
+    MessageActionRow as DiscordMessageActionRow,
+    MessageSelectMenu,
+    SelectMenuInteraction
 } from "discord.js";
 import { inject, injectable } from "tsyringe";
 import chunkBy from "lodash/chunk.js";
@@ -18,13 +22,18 @@ import BaseCommand from "#base/BaseCommand";
 import { kClient, kCommands } from "#utils/tokens";
 import { HelpCommandSub } from "./subcommands/HelpCommand.js";
 import { stripIndents } from "common-tags";
+import { Emojis } from "#utils/constants";
 
 interface CommandCollectionInterface {
     name: string;
     description: string;
     type: string;
-    autoEnabled: boolean;
     category: string;
+}
+
+interface MenuInterface {
+    category: string[];
+    disabled: boolean;
 }
 
 interface StateInterface {
@@ -79,7 +88,6 @@ export default class extends BaseCommand {
                 USER: "User Context Menu",
                 MESSAGE: "Message Context Menu"
             }[m.type],
-            autoEnabled: m.defaultPermission,
             category: this.commands.find((x) => x.name.toLowerCase() === m.name.toLowerCase())?.category || "Other"
         })) as CommandCollectionInterface[];
         const pages = chunkBy(commandsCollection, 5);
@@ -87,23 +95,85 @@ export default class extends BaseCommand {
 
         const msg = (await interaction.followUp({
             embeds: [this.buildPage(this.state.currentPage, 1, pages.length)],
-            components: this.prepareButtons(),
+            components: [
+                ...this.prepareButtons(),
+                this.prepareMenu({
+                    category: [...new Set(commandsCollection.map((m) => m.category))],
+                    disabled: false
+                })
+            ],
             fetchReply: true
         })) as Message<true>;
 
         const event = msg.createMessageComponentCollector({
             componentType: "BUTTON",
-            filter: (x) => x.message.id === msg.id && ["back", "front"].includes(x.customId),
+            filter: (x) => x.message.id === msg.id && ["back", "front", "close", "first", "last"].includes(x.customId),
             time: 60_000
         });
 
-        return this.handleInteraction(event, msg, interaction.user.id, pages);
+        const categoryEvent = msg.createMessageComponentCollector({
+            componentType: "SELECT_MENU",
+            filter: (x) => x.message.id === msg.id && ["commandsCategory"].includes(x.customId),
+            time: 60_000
+        });
+
+        this.handleSelectMenu(categoryEvent, msg, interaction.user.id, pages);
+        return this.handleInteraction(event, categoryEvent, msg, interaction.user.id, pages);
     }
 
-    async handleInteraction(collector: InteractionCollector<ButtonInteraction<"cached">>, message: Message<true>, target: Snowflake, pages: CommandCollectionInterface[][]) {
+    async handleSelectMenu(collector: InteractionCollector<SelectMenuInteraction<"cached">>, message: Message<true>, target: Snowflake, pages: CommandCollectionInterface[][]) {
         collector.on("collect", async (interaction) => {
+            if (interaction.replied) return;
             if (interaction.user.id !== target) {
-                if (interaction.replied) return;
+                const ops = {
+                    embeds: [this.prepareError(`❌ | Only <@${target}> can use this menu.`)],
+                    ephemeral: true
+                };
+
+                if (interaction.deferred) return void (await interaction.followUp(ops));
+                return void (await interaction.reply(ops));
+            }
+
+            switch (interaction.customId) {
+                case "commandsCategory":
+                    {
+                        const newPage = pages.flat().filter((x) => interaction.values.includes(x.category));
+                        const nextPage = this.buildPage(newPage, 1, 1);
+                        this.setState({ currentPage: newPage });
+                        await interaction.deferUpdate();
+                        await interaction.editReply({ embeds: [nextPage] });
+                        collector.resetTimer();
+                    }
+                    break;
+                default:
+                    break;
+            }
+        });
+
+        collector.on("end", async () => {
+            if (!message.deleted && message.editable) {
+                const components = [
+                    ...this.prepareButtons(true),
+                    this.prepareMenu({
+                        disabled: true,
+                        category: ["1"]
+                    })
+                ];
+                await message.edit({ components });
+            }
+        });
+    }
+
+    async handleInteraction(
+        collector: InteractionCollector<ButtonInteraction<"cached">>,
+        c1: InteractionCollector<SelectMenuInteraction<"cached">>,
+        message: Message<true>,
+        target: Snowflake,
+        pages: CommandCollectionInterface[][]
+    ) {
+        collector.on("collect", async (interaction) => {
+            if (interaction.replied) return;
+            if (interaction.user.id !== target) {
                 const ops = {
                     embeds: [this.prepareError(`❌ | Only <@${target}> can use this button.`)],
                     ephemeral: true
@@ -114,6 +184,17 @@ export default class extends BaseCommand {
             }
 
             switch (interaction.customId) {
+                case "first":
+                case "last":
+                    {
+                        const newPage = pages[interaction.customId === "first" ? 0 : pages.length - 1];
+                        const nextPage = this.buildPage(newPage, interaction.customId === "first" ? 1 : pages.length, pages.length);
+                        this.setState({ currentPage: newPage });
+                        await interaction.deferUpdate();
+                        await interaction.editReply({ embeds: [nextPage] });
+                        collector.resetTimer();
+                    }
+                    break;
                 case "back":
                     {
                         const newPage = this.paginate(pages, this.state.currentPage, true);
@@ -134,6 +215,13 @@ export default class extends BaseCommand {
                         collector.resetTimer();
                     }
                     break;
+                case "close":
+                    {
+                        collector.stop();
+                        await interaction.deferUpdate().catch(() => null);
+                        if (interaction.message.deletable) await interaction.message.delete().catch(() => null);
+                    }
+                    break;
                 default:
                     break;
             }
@@ -141,7 +229,13 @@ export default class extends BaseCommand {
 
         collector.on("end", async () => {
             if (!message.deleted && message.editable) {
-                const components = this.prepareButtons(true);
+                const components = [
+                    ...this.prepareButtons(true),
+                    this.prepareMenu({
+                        disabled: true,
+                        category: ["1"]
+                    })
+                ];
                 await message.edit({ components });
             }
         });
@@ -151,11 +245,30 @@ export default class extends BaseCommand {
         return (
             <>
                 <MessageActionRow>
-                    <MessageButton style="SUCCESS" label="Back" emoji="915183448606511145" customId="back" disabled={!!disabled} />
-                    <MessageButton style="SUCCESS" label="Next" emoji="915183144464953364" customId="front" disabled={!!disabled} />
+                    <MessageButton style="PRIMARY" emoji="⏪" customId="first" disabled={!!disabled} />
+                    <MessageButton style="PRIMARY" emoji="915183448606511145" customId="back" disabled={!!disabled} />
+                    <MessageButton style="DANGER" emoji="✖️" customId="close" disabled={!!disabled} />
+                    <MessageButton style="PRIMARY" emoji="915183144464953364" customId="front" disabled={!!disabled} />
+                    <MessageButton style="PRIMARY" emoji="⏩" customId="last" disabled={!!disabled} />
                 </MessageActionRow>
             </>
         ).components;
+    }
+
+    prepareMenu(ops: MenuInterface) {
+        return new DiscordMessageActionRow().addComponents(
+            new MessageSelectMenu()
+                .setDisabled(!!ops.disabled)
+                .addOptions(
+                    ops.category.map((m) => ({
+                        label: m,
+                        value: m,
+                        description: `Commands under ${m} category`
+                    }))
+                )
+                .setCustomId("commandsCategory")
+                .setPlaceholder("Select a category...")
+        );
     }
 
     paginate(pages: CommandCollectionInterface[][], currentPage: CommandCollectionInterface[] | null, back = false) {
@@ -184,26 +297,33 @@ export default class extends BaseCommand {
                 page.map((m) => ({
                     name: m.name,
                     value: stripIndents`${m.description}
-                    **Auto Enabled**: ${m.autoEnabled ? "✅" : "❌"}
-                    **Command Type**: ${m.type}
-                    **Command Category**: ${m.category}`,
+                    ${Emojis.REPLY_BRANCH} Type: **${m.type}**
+                    ${Emojis.REPLY} Category: **${m.category}**`,
                     inline: false
                 }))
             )
-            .setColor("RANDOM")
+            .setColor("BLURPLE")
             .setFooter(`Page ${id} of ${total}`)
             .setTimestamp();
     }
 
-    async getCommands() {
+    async getCommands(guild?: Guild) {
+        const guildCommands = guild
+            ? !guild.commands.cache.size
+                ? await guild.commands.fetch({
+                      force: true,
+                      cache: true
+                  })
+                : guild.commands.cache
+            : null;
         if (!this.client.application.commands.cache.size) {
             const fetched = await this.client.application.commands.fetch({
                 force: true,
                 cache: true
             });
-            return fetched;
+            return guildCommands ? fetched.concat(guildCommands) : fetched;
         } else {
-            return this.client.application.commands.cache;
+            return guildCommands ? this.client.application.commands.cache.concat(guildCommands) : this.client.application.commands.cache;
         }
     }
 
@@ -216,7 +336,7 @@ export default class extends BaseCommand {
                 return false;
             }
             // try fetching command
-            const commands = await this.getCommands();
+            const commands = await this.getCommands(guild);
             if (!commands.size) {
                 if (!interaction.isAutocomplete()) await interaction.followUp({ embeds: [this.prepareError("❌ | Something went wrong: `could not find commands`")] });
                 return false;
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,1 +1,9 @@
 export const RONBAnnouncementChannel = process.env.RONB_ANNOUNCEMENT_CHANNEL;
+
+export enum Emojis {
+    REPLY = "<:reply:915242749136109639>",
+    REPLY_BRANCH = "<:reply_thread:915243432895729674>",
+    LEFT_ARROW = "<:leftarrow:915183448606511145>",
+    RIGHT_ARROW = "<:rightarrow:915183144464953364>",
+    ERROR_DEFAULT = "❌"
+}

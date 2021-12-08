dad6fdaad8d712627a9d49067289271534c1022d
Nishant Sapkota <snishant306@gmail.com>
2021-12-08 17:26:39 +0545

    feat: add `covid` command

---
+++ src/commands/General/CovidCommand.ts
@@ -0,0 +1,75 @@
+import { Client, CommandInteraction, MessageEmbed, MessageActionRow, MessageButton } from "discord.js";
+import { inject, injectable } from "tsyringe";
+import fetch from "node-fetch";
+
+import BaseCommand from "#base/BaseCommand";
+import { kClient } from "#utils/tokens";
+
+@injectable()
+export default class extends BaseCommand {
+    constructor(@inject(kClient) public readonly client: Client<true>) {
+        super({
+            name: "covid",
+            category: "General"
+        });
+    }
+
+    async execute(interaction: CommandInteraction) {
+        const country = interaction.options.getString("country") ?? "Nepal";
+        const data = (await fetch(`https://coronavirus-19-api.herokuapp.com/countries/${country}`).then((res) => res.json())) as any;
+        const actionRow = new MessageActionRow().addComponents(
+            new MessageButton()
+            .setLabel("Stay Safe!")
+            .setURL("https://www.who.int/emergencies/diseases/novel-coronavirus-2019")
+            .setStyle("LINK")
+        )
+        const embed = new MessageEmbed()
+            .setAuthor(`COVID-19 Data of ${data.country}`, this.client.user.displayAvatarURL())
+            .setColor("BLURPLE")
+            .setThumbnail("https://nlihc.org/sites/default/files/pictures/coronavirus.png")
+            .addFields(
+                {
+                    name: "Total Cases",
+                    value: data.cases.toString(),
+                    inline: true
+                },
+                {
+                    name: "Total Deaths",
+                    value: data.deaths.toString(),
+                    inline: true
+                },
+                {
+                    name: "Recovered",
+                    value: data.recovered.toString(),
+                    inline: true
+                },
+                {
+                    name: "Today's Cases",
+                    value: data.todayCases.toString(),
+                    inline: true
+                },
+                {
+                    name: "Today's Deaths",
+                    value: data.todayDeaths.toString(),
+                    inline: true
+                },
+                {
+                    name: "Active Cases",
+                    value: data.active.toString(),
+                    inline: true
+                },
+                {
+                    name: "Critical Cases",
+                    value: data.critical.toString(),
+                    inline: true
+                },
+                {
+                    name: "Total Tests",
+                    value: data.totalTests.toString(),
+                    inline: true
+                }
+            );
+
+        await interaction.reply({ embeds: [embed], components: [actionRow] });
+    }
+}
